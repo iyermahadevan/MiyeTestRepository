ed157acff996878c4b1ac8e0e3c12fa32b8414ed
MIYE <mahadevan.iyer@barco.com>
2017-03-11 05:41:13 -0800

    Initial Commit

---
+++ openCVSamplefacedetection/.externalNativeBuild/ndkBuild/debug/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,125 @@
+C:\miye\workandroid\face-detection\openCVSamplefacedetection\.externalNativeBuild\ndkBuild\debug\obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+  C:/miye/workandroid/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.cpp \
+  C:/miye/workandroid/face-detection/openCVSamplefacedetection/src/main/jni\DetectionBasedTracker_jni.h \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/core.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/hal/interface.h \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/version.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/base.hpp \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/neon_utils.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/saturate.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/fast_math.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types.hpp \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/core_c.h \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types_c.h \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/detection_based_tracker.hpp \
+  /miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h \
+  C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque
+
+C:/miye/workandroid/face-detection/openCVSamplefacedetection/src/main/jni\DetectionBasedTracker_jni.h:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/core.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/hal/interface.h:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/version.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/base.hpp:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/neon_utils.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/saturate.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/fast_math.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types.hpp:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/core_c.h:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types_c.h:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/detection_based_tracker.hpp:
+
+/miye/downloads/opencv/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h:
+
+C:/Users/miye/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque:
